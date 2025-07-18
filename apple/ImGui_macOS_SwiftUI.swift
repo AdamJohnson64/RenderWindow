aadc22eb70fc8c7740009687f4ae7209c88ae4b8
adamjohnson64 <adam.johnson@bwnc.org>
2025-07-17 22:24:04 -0700

    Add in ImGui project.

---
+++ apple/ImGui_macOS_SwiftUI.swift
@@ -0,0 +1,30 @@
+import MetalKit
+import SwiftUI
+
+struct NSViewRepresentableImpl: NSViewRepresentable {
+    var mtkView: MTKView
+    //var renderer: MTKViewDelegate
+
+    init() {
+        mtkView = MTKView(frame: .zero, device: MTLCreateSystemDefaultDevice())
+        //renderer = MTKViewDelegateImpl(device: mtkView.device!)
+        //setupMTKView(view: mtkView, delegate: renderer)
+    }
+
+    func makeNSView(context: Context) -> NSView {
+        return mtkView
+    }
+
+    func updateNSView(_ nsView: NSView, context: Context) {
+        // No-op
+    }
+}
+
+@main
+struct AppImpl: App {
+    var body: some Scene {
+        Window("RenderWindow", id: "RenderWindow") {
+            NSViewRepresentableImpl()
+        }
+    }
+}
